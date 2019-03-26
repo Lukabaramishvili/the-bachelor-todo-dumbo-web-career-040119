20e1e202412a6604e2f49e84291eebffcd909360
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-26 21:49:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,5 +45,5 @@ def get_average_age_for_season(data, season)
     total_age += contestant["age"]
     contestant_total += 1 
   end
-  return (total_age/)
+  return (total_age/contestant_total)
 end
