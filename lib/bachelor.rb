292d307f21603523653714aa5d03264986b29e6a
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-26 21:34:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,8 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  counter = 0 
+  data.each do |contestants, season|
 end
 
 def get_occupation(data, hometown)
