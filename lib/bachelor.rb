90a0c9f93ededf7becea61a90173458b83d66a29
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-26 21:44:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,5 +39,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["age"]
 end
